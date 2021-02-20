997f2a5856c73c24a2dc852fbbfd9e0b457099d5
Carlos J. Rosales Gallegos <carlos@beta-robots.com>
2021-02-20 23:02:50 +0100

    Adds first attemp to automate bibligraphy generation

---
+++ pages/sarscov2/generate_bibs.sh
@@ -0,0 +1,2 @@
+bibtex2html -nofooter -noheader -nokeys  -nokeywords -nodoc -dl -d -s /usr/share/texlive/texmf-dist/bibtex/bst/chicago/chicago.bst -o _therapeutics therapeutics.bib
+mv _therapeutics.html ../../_includes/
